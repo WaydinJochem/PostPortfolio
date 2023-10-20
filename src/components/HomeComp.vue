3ece4df5a174f04419372adc329607cc1001fd3b
WaydinJochem <jochem7405@gmail.com>
2023-10-20 11:49:26 +0200

    New changes still incomplete

---
+++ src/components/HomeComp.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="home row container-fluid p-5">
+        <div class="col my-auto px-5">
+            <h2 class="px-5 ">Hi, my Name's Waydin Jochem.
+                Wanna know more about me why not go browse through my projects or get in contact with me, perhaps my skills
+                might
+                be of use to you
+            </h2>
+            <div class="row px-5 mt-5">
+                <button class="col">CV</button>
+                <button class="col">More</button>
+            </div>
+        </div>
+        <div class="col">
+            <img src="https://i.postimg.cc/sgvMG72r/Pic.jpg" alt="" class="img-fluid">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.home {
+    font-family: 'Alfa Slab One', serif;
+    color: #FBF7F7;
+}
+
+img {
+    border-radius: 50%;
+    height: 761px;
+    width: 617px;
+}
+
+button {
+    padding: 30px;
+    
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,19 @@
 <template>
-  <div class="home">
-    <!-- <div class="starter-text row">
-      <h2 class="col">Hi, my Name’s Waydin Jochem.
-        Wanna know more about me why not go browse through my projects or get in contact with me, perhaps my skills might
-        be of use to you</h2>
-      <img src="https://i.postimg.cc/43LsW471/New-Profile-Image.jpg" alt="" class="col">
-    </div> -->
+  <div>
+    <HomeComp />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import HomeComp from '@/components/HomeComp.vue';
 
 
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    HomeComp
+  }
 }
 </script>
 
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap');
 
-.home {
-  margin: 20dvb;
-  font-family: 'Alfa Slab One', serif;
-  color: #FBF7F7;
-}
-
-img {
-  border-radius: 50%;
-  width: 617px;
-  height: 761px;
-}
-h2{
-  margin: 20em 0;
-}
-
-</style>
