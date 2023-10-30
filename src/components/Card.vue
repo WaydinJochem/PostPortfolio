35f0e7b810092f684d180cf81424bea6db89def4
WaydinJochem <jochem7405@gmail.com>
2023-10-30 16:06:47 +0200

    Card Animations

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,17 @@
+
 <template>
     <div class="skills">
         <div class="crd py-3" style="width: 18rem;" :key="skills.id">
             <img :src="skills.img" :alt="skills.title">
-            <div class="ctn">
+            <div class="ctn" id="ctn">
                 <h4><b>{{ skills.title }}</b></h4>
-                <p>Architect & Engineer</p>
+                <button @click="showDescr()">Description</button>
             </div>
+            <section v-if="Descr"  @click="showDescr()" id="status" class="animate__animated animate__fadeInUp">
+                <div :key="skills.id" class="my-4" style="text-align: center;">
+                    {{ skills.descr }}
+                </div>
+            </section>
         </div>
     </div>
 </template>
@@ -13,6 +19,16 @@
 export default {
     props: {
         skills: Object
+    },
+    data() {
+        return {
+            Descr: false,
+        }
+    },
+    methods: {
+        showDescr() {
+            this.Descr = !this.Descr
+        }
     },
 
 }
@@ -22,18 +38,59 @@ export default {
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     transition: 0.3s;
     border-radius: 20px;
+    position: relative;
 }
 
 .crd:hover {
     box-shadow: 0 8px 16px 0 white;
-    transform: scale(1.25);
+    transform: scale(1.05);
 }
 
 .ctn {
     padding: 2px 16px;
+    position: relative;
 }
 
 img {
     width: 8rem;
+}
+
+section {
+    display: inline;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: .5s ease;
+    background-color: #008CBA;
+    border-radius: 20px;
+    box-shadow: 0 4px 8px 0 #ddd;
+}
+
+/* .crd:hover section {
+    opacity: 1;
+} */
+
+button {
+    display: none;
+}
+
+
+
+.crd:hover button {
+    display: inline;
+
+}
+@keyframes appear {
+    from{
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
 }
 </style>
--- src/components/SkillsComp.vue
+++ src/components/SkillsComp.vue
@@ -1,12 +1,7 @@
 <template>
     <div class="">
         <section id="img" class="py-3 px-5 row " v-if="skills">
-            <Card v-for="skill in skills" :key="skill.id" :skills="skill" id="skills" @click="showDescr()" class="col-4"/>
-        </section>
-        <section v-if="Descr" class="border-white border-start">
-            <div v-for="skill in skills" :key="skill">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ex modi deserunt earum? Repellat officiis eaque incidunt officia! Vitae doloremque, porro consectetur delectus at beatae odit voluptate praesentium pariatur aut.
-            </div>
+            <Card v-for="skill in skills" :key="skill.id" :skills="skill" id="skills" class="col-4"/>
         </section>
     </div>
 </template>
@@ -22,18 +17,7 @@ export default {
         }
     },
     methods: {
-        showDescr() {
-            this.Descr = !this.Descr
-            const pic = document.getElementById("skills")
-            if (this.Descr) {
-                pic.style.display = "grid"
-                pic.style.gridTemplateColumns = "auto auto auto"
-            }
-            else {
-                pic.style.display = "flex"
-                pic.style.flexDirection = "column"
-            }
-        }
+      
     },
     computed: {
         skills() {
@@ -51,30 +35,4 @@ section {
     color: #FBF7F7;
 }
 
-img {
-    width: 8rem;
-}
-
-.crd {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    transition: 0.3s;
-    border-radius: 20px;
-}
-
-.crd:hover {
-    box-shadow: 0 8px 16px 0 white;
-    transform: scale(1.25);
-}
-
-.ctn {
-    padding: 2px 16px;
-}
-
-
-#skills {
-    /* display: grid;
-    grid-template-columns: auto auto auto; */
-    /* display: flex;
-    flex-direction: row; */
-}
 </style>
